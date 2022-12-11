5c5a720eca99ba90eb12db16cfea004e069ee64f
Daniil Iskhakov <shkiper325@gmail.com>
2022-12-11 14:49:40 +0300

    integrate added

---
+++ integrate.cpp
@@ -0,0 +1,19 @@
+#include "integrate.hpp"
+#include "error.hpp"
+
+double integrate(double a, double b, int n, double(*f)(double)) {
+    if (n < 2) error(-1, "n < 2 in integrate");
+
+    double h = (b - a) / n;
+    double len = (b - a);
+
+    double result = 0;
+    for(int i = 0; i < n - 1; ++i) {
+        double x = h * (static_cast<double>(i) + 0.5);
+
+        double val = f(x);
+        result += val * len;
+    }
+
+    return result;
+}
---
+++ integrate.hpp
@@ -0,0 +1,6 @@
+#ifndef INTEGRATE_HPP
+#define INTEGRATE_HPP
+
+double integrate(double a, double b, int n, double(*f)(double));
+
+#endif
