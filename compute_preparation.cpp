eb6d68776c2dd7231bd4d1abf9ad676aa75b0d7a
Daniil Iskhakov <shkiper325@gmail.com>
2022-12-11 15:56:02 +0300

    Added comput_preparation

---
+++ compute_preparation.cpp
@@ -0,0 +1,33 @@
+#include "compute_preparetion.hpp"
+#include "Vec.hpp"
+#include "integrate.hpp"
+#include "hat.hpp"
+#include "error.hpp"
+
+VecD gen_b(int n, int point_count, double(*f)(double)) {
+    if (n % 2 == 1) {
+        error(-1, "Now can generate b ony for even numbers");
+    }
+
+    VecD ret(2 * n - 1);
+    
+    for(int j = 0; j < n; ++j) {
+        auto hat = small_hat(j, n);
+        auto multilpied = [hat = hat, f = f](double x) -> double {
+            return hat(x) * f(x);
+        };
+
+        ret(2 * j) = integrate(static_cast<double>(j), static_cast<double>(j + 1), point_count, multilpied);
+    }
+
+    for (int j = 1; j < n; ++j) {
+        auto hat = big_hat(j, n);
+        auto multilpied = [hat = hat, f = f](double x) -> double {
+            return hat(x) * f(x);
+        };
+
+        ret(2 * j - 1) = integrate(static_cast<double>(j - 1), static_cast<double>(j + 1), point_count, multilpied);
+    }
+
+    return ret;
+}
---
+++ compute_preparation.hpp
@@ -0,0 +1,8 @@
+#ifndef COMPUTE_PREPARATION_HPP
+#define COMPUTE_PREPARATION_HPP
+
+#include "Vec.hpp"
+
+VecD gen_b(int n, int point_count, double(*f)(double));
+
+#endif
