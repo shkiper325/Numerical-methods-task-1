370f9d999cfae2382c328823867e917f2336977a
Daniil Iskhakov <shkiper325@gmail.com>
2022-12-11 15:37:24 +0300

    Integration fixed

--- integrate.cpp
+++ integrate.cpp
@@ -5,14 +5,13 @@ double integrate(double a, double b, int n, double(*f)(double)) {
     if (n < 2) error(-1, "n < 2 in integrate");
 
     double h = (b - a) / (n - 1);
-    double len = (b - a);
 
     double result = 0;
     for(int i = 0; i < (n - 1); ++i) {
         double x = h * (static_cast<double>(i) + 0.5);
 
         double val = f(x);
-        result += val * len;
+        result += val * h;
     }
 
     return result;
