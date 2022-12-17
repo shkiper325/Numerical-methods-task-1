c431f95f8c8f1837d035f040a190cf5217a3ff8d
Daniil Iskhakov <shkiper325@gmail.com>
2022-12-17 16:38:15 +0300

    Fix in compute_preparation & expansion to the borders

--- compute_preparation.cpp
+++ compute_preparation.cpp
@@ -9,7 +9,7 @@ VecD gen_b(int n, int point_count, std::function<double(double)> f) {
         error(-1, "Now can generate b ony for even numbers");
     }
 
-    VecD ret(2 * n - 1);
+    VecD ret(2 * n + 1);
     
     for(int j = 0; j < n; ++j) {
         auto hat = small_hat(j, n);
@@ -17,7 +17,7 @@ VecD gen_b(int n, int point_count, std::function<double(double)> f) {
             return hat(x) * f(x);
         };
 
-        ret(2 * j) = integrate(static_cast<double>(j), static_cast<double>(j + 1), point_count, multilpied);
+        ret(2 * j /**/+ 1) = integrate(static_cast<double>(j) / n, static_cast<double>(j + 1) / n, point_count, multilpied);
     }
 
     for (int j = 1; j < n; ++j) {
@@ -26,8 +26,26 @@ VecD gen_b(int n, int point_count, std::function<double(double)> f) {
             return hat(x) * f(x);
         };
 
-        ret(2 * j - 1) = integrate(static_cast<double>(j - 1), static_cast<double>(j + 1), point_count, multilpied);
+        ret(2 * j - 1 /**/ + 1) = integrate(static_cast<double>(j - 1) / n, static_cast<double>(j + 1) / n, point_count, multilpied);
     }
 
+    {
+        auto hat = big_hat(0, n);
+        auto multilpied = [hat = hat, f = f](double x) -> double {
+            return hat(x) * f(x);
+        };
+
+        ret(0) = integrate(0.0, 1. / n, point_count, hat);
+    }
+
+    {
+        auto hat = big_hat(n, n);
+        auto multilpied = [hat = hat, f = f](double x) -> double {
+            return hat(x) * f(x);
+        };
+
+        ret(2 * n) = integrate(1. - 1. / n, 1., point_count, hat);
+    }
+    
     return ret;
 }
