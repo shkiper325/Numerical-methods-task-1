2ad8a9b4a10f4384ff84f19a096d312b67e1169c
Daniil Iskhakov <shkiper325@gmail.com>
2022-12-21 01:11:14 +0300

    n fixed in hats

--- hats.cpp
+++ hats.cpp
@@ -18,25 +18,25 @@ double gen_hats_scalar_product(int i, int pos, int n) {
         switch (pos) {
         case -2:
             if (i == 0) return 0;
-            else if (i <= n / 2) return 11. / (30. * n) + 2. * n;
+            else if (i <= n) return 11. / (30. * n) + 2. * n;
             else return 11. / (30. * n) + 8. * n / 3.;
         case -1:
             if (i == 0) return 0;
-            else if (i <= n / 2) return 7. / (15. * n) + 8. * n;
+            else if (i <= n) return 7. / (15. * n) + 8. * n;
             else return 7. / (15. * n) + 32. * n / 3.;
         case 0:
             if (i == 0) return 8. / (15. * n) + 2. * n;
-            else if (i <= n / 2 - 1) return 16. / (15. * n) + 4. * n;
-            else if (i == n / 2) return 16. / (15. * n) + 14. * n / 3.;
-            else if (i < n) return 16. / (15. * n) + 16. * n / 3.;
+            else if (i <= n - 1) return 16. / (15. * n) + 4. * n;
+            else if (i == n) return 16. / (15. * n) + 14. * n / 3.;
+            else if (i < 2 * n) return 16. / (15. * n) + 16. * n / 3.;
             else return 8. / (15. * n) + 8. * n / 3.;
         case 1:
-            if (i <= n / 2 - 1) return 7. / (15. * n) + 8. * n;
-            else if (i < n) return 7. / (15. * n) + 32. * n / 3.;
+            if (i <= n - 1) return 7. / (15. * n) + 8. * n;
+            else if (i < 2 * n) return 7. / (15. * n) + 32. * n / 3.;
             else return 0;
         case 2:
-            if (i <= n / 2 - 1) return 11. / (30. * n) + 2. * n;
-            else if (i < n) return 11. / (30. * n) + 8. * n / 3.;
+            if (i <= n - 1) return 11. / (30. * n) + 2. * n;
+            else if (i < 2 * n) return 11. / (30. * n) + 8. * n / 3.;
             else return 0;
         default:
             error(-1, "bad pos given in get_hat");
@@ -46,16 +46,16 @@ double gen_hats_scalar_product(int i, int pos, int n) {
         case -2:
             return 0;
         case -1:
-            if (i <= n / 2 - 1) return 7. / (15. * n) + 2. * n;
-            else if (i < n) return 7. / (15. * n) + 8. * n / 3.;
+            if (i <= n  - 1) return 7. / (15. * n) + 2. * n;
+            else if (i < 2 * n) return 7. / (15. * n) + 8. * n / 3.;
             else return 0;
         case 0:
-            if (i <= n / 2 - 1) return 8. / (15. * n) + 8. * n;
-            else if (i < n) return 8. / (15. * n) + 32. * n / 3.;
+            if (i <= n - 1) return 8. / (15. * n) + 8. * n;
+            else if (i < 2 * n) return 8. / (15. * n) + 32. * n / 3.;
             else return 0;
         case 1:
-            if (i <= n / 2 - 1) return 7. / (15. * n) + 2. * n;
-            else if (i < n) return 7. / (15. * n) + 8. * n / 3.;
+            if (i <= n - 1) return 7. / (15. * n) + 2. * n;
+            else if (i < 2 * n) return 7. / (15. * n) + 8. * n / 3.;
             else return 0;
         case 2:
             return 0;
@@ -67,6 +67,66 @@ double gen_hats_scalar_product(int i, int pos, int n) {
     error(-1, "reached end of gen_hats_scalar_product");
     exit(-1);
 }
+
+
+// double gen_hats_scalar_product(int i, int pos, int n) {
+//     if (n % 2 == 1) {
+//         error(-1, "Now can generate hats ony for even numbers");
+//     }
+
+//     if (i % 2 == 0) { //integrate with big
+//         switch (pos) {
+//         case -2:
+//             if (i == 0) return 0;
+//             else if (i <= n / 2) return 11. / (30. * n) + 2. * n;
+//             else return 11. / (30. * n) + 8. * n / 3.;
+//         case -1:
+//             if (i == 0) return 0;
+//             else if (i <= n / 2) return 7. / (15. * n) + 8. * n;
+//             else return 7. / (15. * n) + 32. * n / 3.;
+//         case 0:
+//             if (i == 0) return 8. / (15. * n) + 2. * n;
+//             else if (i <= n / 2 - 1) return 16. / (15. * n) + 4. * n;
+//             else if (i == n / 2) return 16. / (15. * n) + 14. * n / 3.;
+//             else if (i < n) return 16. / (15. * n) + 16. * n / 3.;
+//             else return 8. / (15. * n) + 8. * n / 3.;
+//         case 1:
+//             if (i <= n / 2 - 1) return 7. / (15. * n) + 8. * n;
+//             else if (i < n) return 7. / (15. * n) + 32. * n / 3.;
+//             else return 0;
+//         case 2:
+//             if (i <= n / 2 - 1) return 11. / (30. * n) + 2. * n;
+//             else if (i < n) return 11. / (30. * n) + 8. * n / 3.;
+//             else return 0;
+//         default:
+//             error(-1, "bad pos given in get_hat");
+//         }
+//     } else {
+//         switch (pos) {
+//         case -2:
+//             return 0;
+//         case -1:
+//             if (i <= n / 2 - 1) return 7. / (15. * n) + 2. * n;
+//             else if (i < n) return 7. / (15. * n) + 8. * n / 3.;
+//             else return 0;
+//         case 0:
+//             if (i <= n / 2 - 1) return 8. / (15. * n) + 8. * n;
+//             else if (i < n) return 8. / (15. * n) + 32. * n / 3.;
+//             else return 0;
+//         case 1:
+//             if (i <= n / 2 - 1) return 7. / (15. * n) + 2. * n;
+//             else if (i < n) return 7. / (15. * n) + 8. * n / 3.;
+//             else return 0;
+//         case 2:
+//             return 0;
+//         default:
+//             error(-1, "bad pos given in get_hat");
+//         }
+//     }
+
+//     error(-1, "reached end of gen_hats_scalar_product");
+//     exit(-1);
+// }
 
 function<double(double)> big_hat(int i, int n) {
     return [n = n, i = i](double x) -> double {
