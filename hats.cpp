50070bb0b2466890e3484c7a93d74eebb931fdef
Daniil Iskhakov <shkiper325@gmail.com>
2023-01-26 22:56:16 +0300

    Fixes i -> i/2 in precomputed scalar products

--- hats.cpp
+++ hats.cpp
@@ -15,47 +15,51 @@ double gen_hats_scalar_product(int i, int pos, int n) {
     }
 
     if (i % 2 == 0) { //integrate with big
+        i /= 2;
+
         switch (pos) {
         case -2:
             if (i == 0) return 0;
-            else if (i <= n) return 11. / (30. * n) + 2. * n;
+            else if (i <= n / 2) return 11. / (30. * n) + 2. * n;
             else return 11. / (30. * n) + 8. * n / 3.;
         case -1:
             if (i == 0) return 0;
-            else if (i <= n) return 7. / (15. * n) + 8. * n;
+            else if (i <= n / 2) return 7. / (15. * n) + 8. * n;
             else return 7. / (15. * n) + 32. * n / 3.;
         case 0:
             if (i == 0) return 8. / (15. * n) + 2. * n;
-            else if (i <= n - 1) return 16. / (15. * n) + 4. * n;
-            else if (i == n) return 16. / (15. * n) + 14. * n / 3.;
-            else if (i < 2 * n) return 16. / (15. * n) + 16. * n / 3.;
+            else if (i <= n / 2 - 1) return 16. / (15. * n) + 4. * n;
+            else if (i == n / 2) return 16. / (15. * n) + 14. * n / 3.;
+            else if (i < n) return 16. / (15. * n) + 16. * n / 3.;
             else return 8. / (15. * n) + 8. * n / 3.;
         case 1:
-            if (i <= n - 1) return 7. / (15. * n) + 8. * n;
-            else if (i < 2 * n) return 7. / (15. * n) + 32. * n / 3.;
+            if (i <= n / 2 - 1) return 7. / (15. * n) + 8. * n;
+            else if (i < n) return 7. / (15. * n) + 32. * n / 3.;
             else return 0;
         case 2:
-            if (i <= n - 1) return 11. / (30. * n) + 2. * n;
-            else if (i < 2 * n) return 11. / (30. * n) + 8. * n / 3.;
+            if (i <= n / 2 - 1) return 11. / (30. * n) + 2. * n;
+            else if (i < n) return 11. / (30. * n) + 8. * n / 3.;
             else return 0;
         default:
             error(-1, "bad pos given in get_hat");
         }
     } else {
+        i /= 2;
+
         switch (pos) {
         case -2:
             return 0;
         case -1:
-            if (i <= n  - 1) return 7. / (15. * n) + 2. * n;
-            else if (i < 2 * n) return 7. / (15. * n) + 8. * n / 3.;
+            if (i <= n / 2 - 1) return 7. / (15. * n) + 2. * n;
+            else if (i < n) return 7. / (15. * n) + 8. * n / 3.;
             else return 0;
         case 0:
-            if (i <= n - 1) return 8. / (15. * n) + 8. * n;
-            else if (i < 2 * n) return 8. / (15. * n) + 32. * n / 3.;
+            if (i <= n / 2 - 1) return 8. / (15. * n) + 8. * n;
+            else if (i < n) return 8. / (15. * n) + 32. * n / 3.;
             else return 0;
         case 1:
-            if (i <= n - 1) return 7. / (15. * n) + 2. * n;
-            else if (i < 2 * n) return 7. / (15. * n) + 8. * n / 3.;
+            if (i <= n / 2 - 1) return 7. / (15. * n) + 2. * n;
+            else if (i < n) return 7. / (15. * n) + 8. * n / 3.;
             else return 0;
         case 2:
             return 0;
@@ -65,9 +69,8 @@ double gen_hats_scalar_product(int i, int pos, int n) {
     }
 
     error(-1, "reached end of gen_hats_scalar_product");
-    exit(-1);
+    std::exit(-1);
 }
-
 
 // double gen_hats_scalar_product(int i, int pos, int n) {
 //     if (n % 2 == 1) {
